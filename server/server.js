8f8b6d2d10236f1ec4fa0dc79f7ffae761b3ad47
Nick Chapman <nickpchapman@gmail.com>
2018-01-22 14:53:47 -0800

    added sorting logic to server endpoint

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('ready to sort'))
+
+app.get('/', (req, res) => {
+  const string = 'zazaza'
+  let sorted = string.split('').sort().join('')
+  res.send(sorted)})
 
 app.listen(3333, () => console.log('now listening on port 3333'))
